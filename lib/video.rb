60b5c770c09208c512794d7e393f617b9af45438
speed-of-light <speed.of.lightt@gmail.com>
2014-02-11 18:17:13 +0800

    add yard gem, document cap class

--- lib/video.rb
+++ lib/video.rb
@@ -1,5 +1,12 @@
+require 'pry'
 
+# @author https://github.com/speed-of-light
 module Video
   require_relative 'video/cap'
 end
+
+puts 'loading complete'
+vf = Video::Cap.new "/home/coder/Desktop/data/pymaging.mp4"
+
 binding.pry
+#vf.dump 0, nil
--- lib/video/cap.rb
+++ lib/video/cap.rb
@@ -1,24 +1,23 @@
+# Class use for capture video
+# @!attribute [r] :stream_path
+#   @return [String] A stream path for capture operations
+# @version 0.1.0
 class Video::Cap
-  require 'opencv'
+  require "opencv"
   include OpenCV
+
+  # get stream path value
   attr_reader :stream_path
   @stream_path = ""
   @cap = nil
-  # @cap methods:
-  # avi_ratio  contrast    format fps=        frames= height  millisecond  query         saturation width
-  # avi_ratio= convert_rgb fourcc frame_count gain    height= millisecond= rectification size       width=
-  # brightness exposure    fps    frames      grab    hue     mode         retrieve      size=
 
-  def set_stream_path(value)
-    @stream_path = value
-    @cap = CvCapture.open(@stream_path)
-    puts "Stream path setted\n#{to_s}"
-  end
-
+  # set stream_path
+  # @param value [String] a consumable string for OpenCV module.
   def stream_path=(value)
     set_stream_path(value)
   end
 
+  # override to string method
   def to_s
     cc = @cap
     "Capture Info:" +
@@ -26,23 +25,46 @@ class Video::Cap
     "\nLength: #{cc.frame_count/cc.fps} seconds"
   end
 
+  # Constructor for cap video
+  # @param sp [String] initialize internal stream path, optional.
   def initialize(sp="")
     set_stream_path(sp) if not sp.empty?
   end
 
+  # Method for dump and manipulate all single frame from a given stream.
+  # @param from [Fixnum] grab the frame from this index.
+  # @param to [Fixnum] grab the frame to this index, use nil to get all.
+  # @return [Array] with given block.
   def dump from, to
     cc = @cap
-    top = cc.frame_count / cc.fps if to.nil?
+    top = (to == nil) ? cc.frame_count / cc.fps : to
     i = from
+    prev = nil
+    a = []
     while i <= top
       cc.millisecond = i * 1000
       break unless cc.grab
       img = cc.retrieve
+      a << yield(img) if block_given?
       puts "val: #{img[12][0]}, ms: #{cc.millisecond}"
       i += 1
     end
+    a
+  end
+
+  # sum and diff with previous image
+  # @param img [OpenCV::lplimage] image variable from #dump
+  # @return [Object] the returned object will fill into array
+  def sum_diff img
+    img.sum
+  end
+
+private
+
+  def set_stream_path(value)
+    @stream_path = value
+    @cap = CvCapture.open(@stream_path)
+    puts "Stream path setted\n#{to_s}"
   end
 end
 
-vf = Video::Cap.new "/home/coder/Desktop/data/pymaging.mp4"
-vf.dump 0, nil
