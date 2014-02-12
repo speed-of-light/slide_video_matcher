a7bf1e556f32c9181cec52dd206d15f14bd40a3c
speed-of-light <speed.of.lightt@gmail.com>
2014-02-12 17:56:23 +0800

    Add features extraction module

--- lib/video.rb
+++ lib/video.rb
@@ -2,7 +2,16 @@ require 'pry'
 
 # @author https://github.com/speed-of-light
 module Video
-  require_relative 'video/cap'
+  require "opencv"
+
+  #require_relative 'video/*'
+  _root = File.dirname(File.absolute_path(__FILE__))
+  Dir.glob(_root + '/video/*') {|file| require file}
+
+  def self.included base
+    base.send :include, FeatHelper
+    #base.extend ClassMethods
+  end
 end
 
 puts 'loading complete'
--- lib/video/cap.rb
+++ lib/video/cap.rb
@@ -4,9 +4,8 @@
 #   @return [String] A stream path for capture operations
 # @version 0.1.0
 class Video::Cap
-  require "opencv"
   include OpenCV
-
+  include Video::FeatHelper
   # get stream path value
   attr_reader :stream_path
   @stream_path = ""
@@ -52,9 +51,12 @@ class Video::Cap
   # @option opts [Integer] :cache_size (1) the size of cache.
   # @option opts [Integer] :interval (1000) the captured frame interval.
   # @return [Array] a collection of handled data
+  # @yield [Hash] Get the cached frames and timestamps for operation
+  # @yieldparam cache [Hash] contained with :img, which is frame data structure, and :ms, the time in milliseconds
+  # @yieldreturn [Object] array item return in the function
   # @see #dump
   # @example Get raw image sum
-  #   cap.cached_dump(from: 0, to: 10, cache_size: 3){|c| cap.check_dump(c) }
+  #   cap.cached_dump(from: 0, to: 10, cache_size: 3){|cache| cap.check_dump(cache) }
   def cached_dump opts={}
     return nil if @cap.nil?
     cc = @cap
@@ -69,7 +71,7 @@ class Video::Cap
     while i <= to
       cc.millisecond = i * iv
       break unless cc.grab
-      ca << cc.retrieve
+      ca << { img: cc.retrieve, ms: cc.millisecond }
       ra << yield( ca ) if block_given?
       puts "Operation on #{cc.millisecond} ms"
       ca.shift 1 if ca.size % cs == 0
@@ -84,27 +86,30 @@ class Video::Cap
   def diff_img
     cached_dump(from: 0, to: 10, cache_size: 2) do |c|
       next if c[1].nil?
-      #sum
-      (c[1].sum - c[0].sum).to_a.inject(0){|mem, obj| mem+obj}.abs
+      diff = (c[1][:img].sum - c[0][:img].sum).to_a.inject(0){|mem, obj| mem+obj}.abs
+      { ms: c[1][:ms], diff: diff }
     end
   end
 
   # @!visibility private
-  def check_dump cache, img
-    puts "cache size: #{cache.size}, img raw: #{img[20][0]}"
+  def check_dump cache
+    puts "cache size: #{cache.size}"
   end
 
+  # Find outliers in given array
+  # @param ary [Array] given array
+  # @param type [Symbol, Number] find :mild outliers or :extreme outliers, use integer to get custom fence
+  # @return [Array] outliers
+  # @see http://en.wikipedia.org/wiki/Outlier
   def outliers ary=[], type=:mild
     iqq = { mild: 1.5, extreme: 3 }
-    iq  = type.class == Fixnum ? type : iqq[type]
-    sa  = ary.sort
-    q1  = percentile sa, 0.25
-    mid = percentile sa, 0.5
-    q3  = percentile sa, 0.75
+    iq  = (type.class == Float || type.class == Fixnum) ? type : iqq[type]
+    sa  = ary.sort{|a, b| a[:diff]<=>b[:diff] }
+    q1  = percentile(sa, 0.25)[:diff]
+    q3  = percentile(sa, 0.75)[:diff]
     iqr = (q3 - q1)*iq
-    #fence = (q1 - iqr)..(q3 + iqr)
     fence = q3+iqr
-    o = ary.reject{|v| v < fence}
+    o = ary.reject{|v| v[:diff] < fence }
   end
 
 private
@@ -115,11 +120,12 @@ private
     puts "Stream path setted\n#{to_s}"
   end
 
+  # multiply items in the array by the required percentile
+  # (e.g. 0.75 for 75th percentile), round the result up to the next whole number
+  # @param ary [Array] input array
+  # @param percent [Float] percentile position
+  # @return subtract one to get the array item we need
   def percentile(ary=[], percent=0.0)
-    # multiply items in the array by the required percentile
-    # (e.g. 0.75 for 75th percentile)
-    # round the result up to the next whole number
-    # then subtract one to get the array item we need to return
     ary ? ary[((ary.length * percent).ceil)-1] : nil
   end
 end
